eb42733cc7c7e8d00ed179de38653b7ae27b434f
Jameel Al-Aziz <jameel@scopely.com>
2013-01-10 17:54:39 -0800

    Added a parameterless constructor to Record and exposed the property setters

--- Leaderboard/Record.cs
+++ Leaderboard/Record.cs
@@ -2,10 +2,12 @@
 {
     public class Record<K, V, T>
     {
-        public K Member { get; protected set; }
-        public V Score { get; protected set; }
-        public long Rank { get; protected set; }
-        public T Data { get; protected set; }
+        public K Member { get; set; }
+        public V Score { get; set; }
+        public long Rank { get; set; }
+        public T Data { get; set; }
+
+        public Record() {}
 
         public Record(K member, V score, long rank, T data = default(T))
         {
